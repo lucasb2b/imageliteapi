e729cd817c0aa1105c4e02bce92c4c762958bb63
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2025-02-28 21:11:47 -0300

    Criando a camada de serviço

--- src/main/java/io/github/lucasb2b/imageliteapi/ImageliteapiApplication.java
+++ src/main/java/io/github/lucasb2b/imageliteapi/ImageliteapiApplication.java
@@ -1,33 +1,12 @@
 package io.github.lucasb2b.imageliteapi;
 
-import io.github.lucasb2b.imageliteapi.domain.entity.Image;
-import io.github.lucasb2b.imageliteapi.domain.enums.ImageExtension;
-import io.github.lucasb2b.imageliteapi.infra.repository.ImageRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class ImageliteapiApplication {
-
-	@Bean
-	public CommandLineRunner commandLineRunner(@Autowired ImageRepository repository){
-		return args -> {
-			Image image = Image
-					.builder()
-					.extension(ImageExtension.PNG)
-					.name("myimage")
-					.tags("teste")
-					.size(1000L)
-					.build();
-
-			repository.save(image);
-		};
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ImageliteapiApplication.class, args);
---
+++ src/main/java/io/github/lucasb2b/imageliteapi/domain/service/ImageService.java
@@ -0,0 +1,7 @@
+package io.github.lucasb2b.imageliteapi.domain.service;
+
+import io.github.lucasb2b.imageliteapi.domain.entity.Image;
+
+public interface ImageService {
+    Image save(Image image);
+}
