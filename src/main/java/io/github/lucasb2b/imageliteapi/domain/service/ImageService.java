e6fe4de3d197d1dab6dad89f7b56e84e6541bcc7
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2025-03-02 16:14:14 -0300

    Criando URL para acesso da imagem para renderização

--- src/main/java/io/github/lucasb2b/imageliteapi/domain/service/ImageService.java
+++ src/main/java/io/github/lucasb2b/imageliteapi/domain/service/ImageService.java
@@ -2,6 +2,10 @@ package io.github.lucasb2b.imageliteapi.domain.service;
 
 import io.github.lucasb2b.imageliteapi.domain.entity.Image;
 
+import java.util.Optional;
+
 public interface ImageService {
     Image save(Image image);
+
+    Optional<Image> getById(String id);
 }
