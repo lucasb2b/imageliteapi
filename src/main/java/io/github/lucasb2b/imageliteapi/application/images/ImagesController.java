dea088cf5a0411ec7e99389b393b44e961f9c255
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2025-03-01 22:06:59 -0300

    Salvando a imagem através da API

--- src/main/java/io/github/lucasb2b/imageliteapi/application/images/ImagesController.java
+++ src/main/java/io/github/lucasb2b/imageliteapi/application/images/ImagesController.java
@@ -1,9 +1,14 @@
 package io.github.lucasb2b.imageliteapi.application.images;
 
 
+import io.github.lucasb2b.imageliteapi.domain.entity.Image;
+import io.github.lucasb2b.imageliteapi.domain.enums.ImageExtension;
+import io.github.lucasb2b.imageliteapi.domain.service.ImageService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,24 +16,36 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/images")
 @Slf4j
+@RequiredArgsConstructor
+
 public class ImagesController {
 
-    private static final Logger log = LoggerFactory.getLogger(ImagesController.class);
+    private final ImageService service;
 
     @PostMapping
     public ResponseEntity save(
             @RequestParam("file") MultipartFile file,
             @RequestParam("name") String name,
             @RequestParam("tags") List<String> tags
-            ){
+            ) throws IOException {
         log.info("Imagem recebida: name: {}, size: {}", file.getOriginalFilename(), file.getSize());
-        log.info("Nome definido para a imagem: {}", name);
-        log.info("Tags: {}", tags);
+
+        Image image = Image.builder()
+                        .name(name)
+                        .tags(String.join(",", tags))
+                        .size(file.getSize())
+                        .extension(ImageExtension.valueOf(MediaType.valueOf(file.getContentType())))
+                        .file(file.getBytes())
+                        .build();
+
+        service.save(image);
+
         return ResponseEntity.ok().build();
     }
 }
