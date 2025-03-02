936e734a5e2f1c4263d605fbe217a21e61606b5e
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2025-03-02 16:28:00 -0300

    Criando a lógica de pesquisa no repositório

--- src/main/java/io/github/lucasb2b/imageliteapi/infra/repository/ImageRepository.java
+++ src/main/java/io/github/lucasb2b/imageliteapi/infra/repository/ImageRepository.java
@@ -1,7 +1,15 @@
 package io.github.lucasb2b.imageliteapi.infra.repository;
 
 import io.github.lucasb2b.imageliteapi.domain.entity.Image;
+import io.github.lucasb2b.imageliteapi.domain.enums.ImageExtension;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ImageRepository extends JpaRepository<Image, String> {
+import java.util.List;
+
+public interface ImageRepository extends JpaRepository<Image, String>, JpaSpecificationExecutor<Image> {
+
+    default List<Image> findByExtensionAndNameOrTagsLike(ImageExtension extension, String query){
+        return findAll();
+    }
 }
