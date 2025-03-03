e2e5ec6e2bd7957bb419284c24098368d9fa66a1
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2025-03-02 21:21:04 -0300

    Corrigindo problemas da pesquisa de imagens

--- src/main/java/io/github/lucasb2b/imageliteapi/application/images/ImagesController.java
+++ src/main/java/io/github/lucasb2b/imageliteapi/application/images/ImagesController.java
@@ -65,9 +65,9 @@ public class ImagesController {
 
     @GetMapping
     public ResponseEntity<List<ImageDTO>> search(
-            @RequestParam(value = "extension", required = false) String extension,
+            @RequestParam(value = "extension", required = false, defaultValue = "") String extension,
             @RequestParam(value= "query", required = false) String query){
-        var result = service.search(ImageExtension.valueOf(extension), query);
+        var result = service.search(ImageExtension.ofName(extension), query);
         var images = result.stream().map(image -> {
             var url = buildImageURL(image);
             return mapper.imageToDTO(image, url.toString());
@@ -79,7 +79,7 @@ public class ImagesController {
     private URI buildImageURL(Image image){
         String imagePath = "/" + image.getId();
         return ServletUriComponentsBuilder
-                .fromCurrentRequest()
+                .fromCurrentRequestUri()
                 .path(imagePath)
                 .build().toUri();
     }
