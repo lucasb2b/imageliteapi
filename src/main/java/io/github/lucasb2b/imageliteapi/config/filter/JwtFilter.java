415a5cf3696785f0966c115b161b5691e56c30fa
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2025-05-04 11:23:03 -0300

    Configuração de filtro para urls abertas

--- src/main/java/io/github/lucasb2b/imageliteapi/config/filter/JwtFilter.java
+++ src/main/java/io/github/lucasb2b/imageliteapi/config/filter/JwtFilter.java
@@ -67,4 +67,9 @@ public class JwtFilter extends OncePerRequestFilter {
         }
         return null;
     }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        return request.getRequestURI().contains("/v1/users");
+    }
 }
